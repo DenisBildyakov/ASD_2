d788c9c291e90c7526b15cc19f524a8907d8e230
Denis Bildyakov <ezoman@yandex.ru>
2024-02-25 23:38:31 +0400

    SimpleTree

--- src/main/java/SimpleTree/SimpleTree.java
+++ src/main/java/SimpleTree/SimpleTree.java
@@ -2,9 +2,9 @@ import java.util.*;
 
 public class SimpleTreeNode<T>
 {
-    public T NodeValue; // значение в узле
-    public SimpleTreeNode<T> Parent; // родитель или null для корня
-    public List<SimpleTreeNode<T>> Children; // список дочерних узлов или null
+    public T NodeValue;
+    public SimpleTreeNode<T> Parent;
+    public List<SimpleTreeNode<T>> Children;
 
     public SimpleTreeNode(T val, SimpleTreeNode<T> parent)
     {
